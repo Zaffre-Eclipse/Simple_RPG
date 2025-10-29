110d1c7aef1ae96ebc71c653400ee323ffa36247
Anner246 <annerarevalo246@gmail.com>
2025-10-29 19:56:30 -0400

    Update Character.py
    Adjusted stats to be more manageable
    Added items and armor dicts
    Added item pickup method

--- Simple_RPG/Character.py
+++ Simple_RPG/Character.py
@@ -1,13 +1,28 @@
 class Character:
+    
     def __init__(self):
         self.currentPosition = "Start"
-
+        
         # --- Status Variables ---
         self.name = "PAWN"
         self.title = "Red Archon"
-        self.hp = 500
-        self.max_hp = 500
-        self.mp = 100
-        self.atk = 120
-        self.defense = 90
-        self.spd = 100
+        self.hp = 50
+        self.mp = 50
+        self.max_hp = 50
+        self.max_mp = 50
+        self.atk = 12
+        self.defense = 9
+        self.spd = 10
+        
+        self.equipment = {"Armor": ("Basic Armor", 0,),
+                          "Weapon": ("Basic Weapon", 0)
+                         }
+        
+        self.items = {"HP Potion": ("HP Potion", 0), 
+                      "MP Potion": ("MP Potion", 0)
+                     }
+        
+    def pickup(self, name:str):
+        item_name, amount = self.items[name]   
+        self.items[name] = (item_name, amount+1) 
+
